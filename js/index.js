36297fee3821d2cc240d55db238deab7920201fa
lukefrostwalker <116066162+lukefrostwalker@users.noreply.github.com>
2022-12-12 00:42:04 +0800

    1.2

---
+++ js/index.js
@@ -0,0 +1,184 @@
+const planContainer = document.querySelector("#planContainer");
+const selectDate = document.querySelectorAll("#dateSelector");
+const subTotal = document.querySelectorAll("#subTotal");
+
+let cart = []
+
+
+function dateSelect(date) {
+    console.log(date)
+}
+
+const dateClicked = e => {
+    let date = e.target.dataset.date
+    let order = e.target.dataset.seq
+    console.log(order)
+    e.target.style.color = "black"
+    e.target.classList.add("clicked")
+    e.target.style.pointerEvents = "none"
+
+    if (e.target.classList.contains("clicked")) {
+        dateSelect(date)
+        e.target.classList.add("delete")
+
+        var main = document.createElement("div")
+        main.classList = "mb-3 deleteID"
+        main.dataset.id = date
+        main.style.order = order
+   
+        var dateTitle = document.createElement("p")
+        dateTitle.classList = "date_title fw-bold text-center py-2 text-white"
+        dateTitle.innerHTML = date + " MENU   "
+
+        var delBtn = document.createElement("button")
+        delBtn.innerHTML = "Remove"
+        delBtn.style.fontSize = "12px"
+        delBtn.classList = "del-btn btn btn-outline-light rounded-pill fw-bold";
+        delBtn.addEventListener("click", delItem)
+    
+        var mealGroup = document.createElement("div")
+        mealGroup.classList = "card-group gap-4"
+  
+        planContainer.appendChild(main)
+        main.appendChild(dateTitle)
+        dateTitle.appendChild(delBtn)
+        main.appendChild(mealGroup)
+
+        meals.forEach((meal) => {
+            mealGroup.innerHTML += `
+            <div class="card col-lg-12 col-sm-3 col-3 border" style="width: 18rem;">
+            <img src="${meal.imgSrc}" alt="...">
+            <div class="card-body position-relative">
+              <h5 class="card-title">
+                ${meal.name}</br><span class="small">₱${meal.price}</span>⠀<i type="button" class="fa-solid fa-circle-info" data-bs-toggle="modal" data-bs-target="#staticBackdrop${meal.id}"></i>
+              </h5>
+              
+              <div class="modal fade" id="staticBackdrop${meal.id}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="staticBackdropLabel">${meal.name}</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="container pb-4">
+                            <div class="text-center" id="meal-modal-image"><img src="${meal.imgSrc}" class="img-thumbnail" alt="meal-image"></div>
+
+                            <div class="text-center alert alert-danger" role="alert">
+                                <strong style="color:red; font-size: 0.825rem;">
+                                    Ingredient items and nutrition facts are subject to change without prior notice.
+                                </strong>
+                            </div>
+                            <div class="mb-3">
+                                <div class="mb-1">
+                                    <span class="fw-bold">Ingredients: </span>
+                                </div>
+                                <div id="ingredients">${meal.ingredients}</div>
+                            </div>
+                            <div class="container">
+                            <div class="py-2">
+                                <div class="mb-2 fw-bold">
+                                    <span>Nutrition</span>
+                                </div>
+                                <div class="nutrition mb-1">
+                                    ${meal.facts}
+                                </div>
+                            </div>
+                            </div>
+                            <div class="d-grid mt-3">
+                                <button type="button" class="btn btn-success" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <p class="card-text">${meal.description}</p>
+
+                <div class="d-inline-flex">
+                    <div class="input-group-sm d-inline-flex">
+                        <button class="btn btn-outline-success" type="button" onclick="changeNumberOfQuantity('minus', ${meal.id})" data-target=${meal.id}"><i class="fa-solid fa-minus"></i></button>
+                        <input type="text" class="form-control quantity-input" placeholder="Quantity" id=${meal.id} value=0>
+                        <button class="btn btn-outline-success" type="button" onclick="changeNumberOfQuantity('plus', ${meal.id})" data-target=${meal.id}"><i class="fa-solid fa-plus"></i></button>
+                    </div>
+                </div>
+            </div>
+          </div>
+            `
+            
+        })
+
+        } else if (e.target.classList.contains("delete")) {
+            // var delMain = document.querySelector('[data-id = ${date}]');
+            // console.log(delMain)
+            console.log(date)
+            // delMain.remove()
+            
+    }
+
+    
+
+    function delItem() {
+        let delText = "Are you sure?";
+            
+            if (confirm(delText) === true) {
+                delBtn.parentElement.parentElement.remove();
+                e.target.classList.remove("clicked")
+                e.target.style.pointerEvents = ""
+            }
+    }
+}
+
+for (let x of selectDate) {
+    x.addEventListener("click", dateClicked)
+}
+
+function changeNumberOfQuantity(action, id) {
+    var quantity = document.querySelector(".quantity-input")
+    quantity.value = 0
+    subTotal.innerHTML = 0
+
+        if(cart.some((item) => item.id === id)) {
+            let numberOfUnits = meals.numberOfUnits
+                quantity.value = numberOfUnits
+            if(meals.id === id) {
+                
+                if (action === "minus" && numberOfUnits > 1) {
+                    quantity.value--
+                    numberOfUnits--
+                } else if (action === "plus") {
+                    numberOfUnits++
+                    quantity.value++
+
+                }
+            }
+        } else {
+            const item = meals.find((meal) => meal.id === id)
+            cart.push({
+                ...item,
+                numberOfUnits: 1,
+            })
+        }
+        console.log(cart)
+        
+   
+    cart = cart.map((meal) => {
+        let numberOfUnits = meal.numberOfUnits
+        if(meal.id === id) {
+            if (action === "minus" && numberOfUnits > 1) {
+                numberOfUnits--
+                // quantity.value--
+            } else if (action === "plus") {
+                numberOfUnits++
+                // quantity.value++
+            }
+        }
+        return {
+            ...meal,
+            numberOfUnits,
+        }
+        
+    })
+}
+
---
+++ js/meals.js
@@ -0,0 +1,78 @@
+const meals = [
+    {
+        id: 0,
+        name: "Cocoa-spiced Beef Tenderloin",
+        description: "Latin American flavors come alive in this festive beef dish with fruity salsa.",
+        price: 799,
+        facts: `<ul class="meal_facts">
+        <li><span>Calories</span> <span>188</span></li>
+        <li><span>Total Fat</span> <span>8 g</span></li>
+        <li><span>Saturated Fat</span> <span>3 g</span></li>
+        <li><span>Cholesterol</span> <span>80 mg</span></li>
+        <li><span>Sodium</span> <span>246 mg</span></li>
+        <li><span>Total Fiber</span> <span>0 g</span></li>
+        <li><span>Protein</span> <span>26 g</span></li>
+        <li><span>Carbohydrate</span> <span>1 g</span></li>
+        <li><span>Potassium</span> <span>390 mg</span></li>
+        </ul>`,
+        imgSrc: "../img/beef-tenderloin.png",
+        ingredients: "Pork Tenderloin, Garlic, Ginger, Fish Sauce, Lite Soy Sauce, Sugar, Sesame Oil",
+    },
+    {
+        id: 1,
+        name: "Turkey Club Burger",
+        description: "Lighten up your traditional hamburger with lean ground turkey--less saturated fat, without less flavor.",
+        price: 499,
+        facts: `<ul class="meal_facts">
+        <li><span>Calories</span><span>299</span></li>
+        <li><span>Total Fat</span> <span>11 g</span></li>
+        <li><span>Saturated Fat</span> <span>2 mg</span></li>
+        <li><span>Cholesterol</span> <span>89 mg</span></li>
+        <li><span>Sodium</span> <span>393 mg</span></li>
+        <li><span>Total Fiber</span> <span>5 g</span></li>
+        <li><span>Protein</span> <span>29 g</span></li>
+        <li><span>Carbohydrate</span> <span>26 g</span></li>
+        <li><span>Potassium</span> <span>424 mg</span></li>
+        </ul>`,
+        imgSrc: "../img/turkey-club-burger.jpg",
+        ingredients: '<span class="fst-italic">For turkey burgers:</span><p>99% Fat-Free Ground Turkey, Scallions, Ground Black Pepper, Egg, Olive Oil</p><span class="fst-italic">For spread:</span><p>Light Mayonnaise, Dijon Mustard</p><span class="fst-italic">For toppings:</span><p>Arugula, Portabella Mushroom, Whole-Wheat Hamburger Buns</p>'
+    },
+    {
+        id: 2,
+        name: "Grilled Tuna and Spinach Salad",
+        description: "Tuna is a sturdy fish that is easy on the stomach.",
+        price: 799,
+        facts: `<ul class="meal_facts">
+        <li><span>Calories</span> <span>282</span></li>
+        <li><span>Total Fat</span> <span>10 g</span></li>
+        <li><span>Saturated Fat</span> <span>2 mg</span></li>
+        <li><span>Cholesterol</span> <span>42 mg</span></li>
+        <li><span>Sodium</span> <span>418 mg</span></li>
+        <li><span>Total Fiber</span> <span>5 g</span></li>
+        <li><span>Protein</span> <span>31 g</span></li>
+        <li><span>Carbohydrate</span> <span>15 g</span></li>
+        <li><span>Potassium</span> <span>874 mg</span></li>
+        </ul>`,
+        imgSrc: "../img/tuna.jpg",
+        ingredients: '<span class="fst-italic">Main:</span><p>Olive Oil, Garlic, Lemon Juice, Oregano, Tuna Steaks</p><span class="fst-italic">For salad:</span><p>Low-Sodium Chickpeas, Spinach, Lemon Juice, Tomato, Salt, Ground Black Pepper</p>',
+    },
+    {
+        id: 3,
+        name: "Cold Fusilli Pasta",
+        description: "A whole new twist on pasta salad!",
+        price: 399,
+        facts: `<ul class="meal_facts">
+        <li><span>Calories</span> <span>418</span></li>
+        <li><span>Total Fat</span> <span>11 g</span></li>
+        <li><span>Saturated Fat</span> <span>3 mg</span></li>
+        <li><span>Cholesterol</span> <span>10 mg</span></li>
+        <li><span>Sodium</span> <span>455 mg</span></li>
+        <li><span>Total Fiber</span> <span>13 g</span></li>
+        <li><span>Protein</span> <span>21 g</span></li>
+        <li><span>Carbohydrate</span> <span>63 g</span></li>
+        <li><span>Potassium</span> <span>576 mg</span></li>
+        </ul>`,
+        imgSrc: "../img/pasta.jpg",
+        ingredients: "Whole-Wheat Fusilli(spiral), Cherry Tomatoes, Green Bell Pepper, Red Onion, Zucchine, Low-Sodium Chickpeas, Fresh Basil, Salt, Ground Black Pepper, Extra Virgin Olive Oil, Balsamic Vinegar, Parmesan Cheese",
+    }
+]
